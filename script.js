2d961f564e762a474ccb7b1cc4d09c5cdee43c58
Marcelo de Moraes <marcelomor546@gmail.com>
2022-06-05 15:12:46 -0300

    Desafio feito, sprint 2 jogo da forca

--- script.js
+++ script.js
@@ -5,62 +5,93 @@ let pageOne = document.querySelector(".page-one");
 let pageTwo = document.getElementById("page-two").style.display = "none";
 let pageThree = document.querySelector(".page-three").style.display = "none";
 let adicionarPalavra = document.getElementById("adiciona");
-// const maximoLetras = document.querySelector(".maximo-letras").style.display = "none";
-// const salvarPalavra = document.querySelector(".salvar-palavra").style.display = "none";
-// const cancelarJogo = document.querySelector(".cancelar").style.display = "none";
-// const teaPalavra = document.querySelector(".textarea-palavra").style.display = "none";
-const palavras = ["cahorro", "alura", "gato", "labora", "elefante",
- "thor", "scooby", "nenem", "oracle"];
- const palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)];
- const letrasCertas = [];
- const letrasErradas = [];
+let palavras = ["cachorro", "alura", "gato", "labora", "elefante",
+ "one", "java", "sprint", "oracle", "challenge", "casa", "carro"];
+ let palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)];
 
- document.addEventListener("keydown", (event) => {
-     const codigoLetra = event.keyCode; 
-     if(isLetra(codigoLetra)){
-         const letra = event.key;
-         if(letrasErradas.includes(letra)){
-             mostrarAvisoLetra();
-         }else{
-             if(palavraSecreta.includes(letra)){
-                 letrasCertas.push(letra);
-             }else{
-                 letrasErradas.push(letra);
-             }
-         }
-        atualizaJogo();
-     }
- })
+const letrasCertas = [];
+const letrasErradas = [];
 
-//Funções
+let display = document.getElementById("display");
+
+function checaInput(letra){
+    letra = letra.toLowerCase()
+    if(letrasErradas.includes(letra)){
+        
+    }else{
+        if(palavraSecreta.includes(letra)){
+            letrasCertas.push(letra);
+            
+        }else{
+            letrasErradas.push(letra);
+            
+        }
+    }
+   atualizaJogo();
+}
+
+function isLetra(codigo) {
+    
+    return codigo >= 65 && codigo <= 90;
+}
 
 function atualizaJogo(){
+    
     mostraLetrasErradas();
     mostrarLetrasCertas();
     desenhaForca();
-
+    verificaresultado();
 }
 
 function mostraLetrasErradas(){
-    const div = document.querySelector(".letras-erradas");
-    div.innerHTML = "";
+    const conteinerErro = document.querySelector(".letras-erradas");
+    conteinerErro.innerHTML = "";
     letrasErradas.forEach(letra => {
-        div.innerHTML += "  " + letra;
+        conteinerErro.innerHTML += "  " + letra;
     });
 }
 
 function mostrarLetrasCertas() {
+    
     const container = document.querySelector(".letras-certas");
-    container.innerHTML = "";
+    container.innerHTML ="";
     palavraSecreta.split("").forEach(letra => {
+          
         if(letrasCertas.includes(letra)) {
-            container.innerHTML += " " + letra;
+            container.innerHTML += letra;
             
         }else {
-            container.innerHTML += "  " + " _" ;
+            container.innerHTML +=" _" ;
             
         }
+        
     })
+}
+
+function verificaresultado(){
+    const container = document.getElementById("display").innerText;
+    const parteCorpo = document.querySelectorAll(".forca-parte");
+    
+    let mensagem = "";
+    
+    if(palavraSecreta.toUpperCase() === container){
+               
+        document.querySelector(".page-three").style.display = "none";
+        mensagem = "Você Venceu." +"<br><br>"+ " Parabéns!";
+        document.getElementById("popu").style.color = "#15f409";
+    }
+   
+    if(letrasErradas.length === parteCorpo.length){
+        mensagem = "Fim de Jogo, você perdeu." + "<br><br>" + "A palavra secreta é: " + "<br><br>" + "[ "+ palavraSecreta.toLocaleUpperCase() + " ]";
+        document.querySelector(".page-three").style.display = "none";
+        document.getElementById("popu").style.color = "#bc1b68";
+              
+    }
+       
+    if(mensagem){
+        document.querySelector("#mensagem").innerHTML = mensagem;
+        document.querySelector(".mensagem-popup").style.display = "flex";
+    }
 }
 
 function desenhaForca() {
@@ -69,30 +100,41 @@ function desenhaForca() {
         parteCorpo[i].style.display = "block";
     }
 }
-
-function mostrarAvisoLetra(){
-    const aviso = document.querySelector(".aviso");
-    aviso.classList.add("show");
-    setTimeout(() => {
-        aviso.classList.remove("show");
-
-    }, 1000);
-
-}
-
-function isLetra(codigo) {
-    return codigo >= 65 && codigo <= 90;
-}
-
     
 function comecarJogo() {
     pageOne.style.display = "none";
     pageThree = document.querySelector(".page-three").style.display = "block";
     atualizaJogo();
-    console.log(palavraSecreta);
-    
 
+    document.addEventListener("keydown", (event) => {
+    if(isLetra(event.keyCode)){
+            checaInput(event.key);
+        }
+     })
+
+    const btn = document.querySelectorAll(".letras-teclado");
+
+    btn.forEach(function(button) {
+      button.addEventListener('click', function(){ 
+        checaInput(button.innerHTML)
+      });
+    });
+    console.log(palavras);
+    console.log(palavraSecreta);
+}
+
+ function salvarPalavra(){
+     
+    let palavra = document.querySelector(".textarea-palavra");
     
+    if(palavra.value == ""){
+       alert("Digite uma palavra ou frase:"); 
+    }else{
+        palavras.push(palavra.value);
+        document.getElementById("page-two").style.display = "none";
+        document.querySelector(".page-three").style.display = "block";
+        comecarJogo();
+    }
 }
 
 function adicionaPalavra() {
@@ -103,7 +145,6 @@ function adicionaPalavra() {
 }
 
 function cancelarPalavra() {
-    //pageTwo.style.display = "none";
     pageTwo = document.getElementById("page-two").style.display = "none";
     pageOne.style.display = "block";
     
@@ -112,5 +153,11 @@ function cancelarPalavra() {
 function desistir(){
     pageThree = document.querySelector(".page-three").style.display = "none";
     pageOne.style.display = "block";
+    window.location.reload();
+ 
+}
 
+function novoJogo(){
+    window.location.reload();
+       
 }
